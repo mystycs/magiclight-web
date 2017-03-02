c3a0535619dbe7d0bddfc3e641af6c6e8341303d
Mark <mystycs@gmail.com>
2017-03-02 01:56:29 -0500

    delete old migration

--- db/migrate/20170301192252_create_magic_lights.rb
+++
@@ -1,9 +0,0 @@
-class CreateMagicLights < ActiveRecord::Migration
-  def change
-    create_table :magic_lights do |t|
-      t.string :ip_address
-
-      t.timestamps null: false
-    end
-  end
-end
