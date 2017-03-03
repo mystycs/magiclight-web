32ea1b5d9d3a3df03c552a8244c7f4f1ccbbe7c0
Mark <mystycs@gmail.com>
2017-03-02 23:03:50 -0500

    cleanup changecolor method/algo

--- app/models/color.rb
+++ app/models/color.rb
@@ -5,22 +5,17 @@ class Color < ActiveRecord::Base
     host = light_ip
     # raise color
     port = 5577
-    s = TCPSocket.new(host, port)
-    color = color.to_s[1..-1]
-    colorstring = '31' + color + '00F00F'
-    colorcode = [colorstring].pack 'H*'
-    byteArray = colorcode.bytes
+    colorstring = '31' + color.to_s[1..-1] + '00F00F'
+    byteArray = [colorstring].pack 'H*'
 
     sumOfValues = 0
-    byteArray.each do |x|
+    byteArray.bytes.each do |x|
       sumOfValues += x
     end
 
-    checksum = sumOfValues % (2**(byteArray.length + 1))
-    checksum = checksum.to_s(16)
-    colorstring += checksum
-    colorcode = [colorstring].pack 'H*'
+    colorstring += (sumOfValues % (2**(byteArray.length + 1))).to_s(16)
 
+    s = TCPSocket.new(host, port)
     s.write [colorstring].pack 'H*'
     s.close
   end
